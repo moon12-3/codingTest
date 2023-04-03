696f4336d2855276c44ab4f4a4d39b37c1a5416f
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2023-04-04 08:49:10 +0900

    문자열 내 마음대로 정렬하기

---
+++ sortOfMe.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (x, y) -> {
+            if(x.charAt(n)-y.charAt(n)==0) return x.compareTo(y);
+            else return x.charAt(n)-y.charAt(n);
+            });
+        return strings;
+    }
+}
