da8036da704a3bff0e04b7ea8b0a13538e906731
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2023-04-18 19:28:29 +0900

    둘만의 암호(수정)

--- OnlyTwoPass.java
+++ OnlyTwoPass.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-        char a = 'c';
         for(int i = 0; i < s.length(); i++) {
             int c = index;
+            char a = s.charAt(i);
             for(int j = 1; j <= c; j++) {
-                a = (char)(s.charAt(i)+j);
-                if(a>'z') a=(char)(('a'-1)+a%'z');
+                a++;
                 if(a>'z') a=(char)(('a'-1)+a%'z');
                 for(int z = 0; z < skip.length(); z++) {
                     if(a==skip.charAt(z)) {
